81a15747dd0df261bcc269e45bc9785d9e1eb8a3
Jakub Kaderka <jakub.kaderka@gmail.com>
2019-01-13 11:08:27 +0100

    Cleaned up the main for ecu

--- fw/sources/app/ecu/main.c
+++ fw/sources/app/ecu/main.c
@@ -29,85 +29,37 @@
 #include <hal.h>
 #include <chprintf.h>
 
+#include <drivers/wdg.h>
 #include <modules/log.h>
 #include <modules/config.h>
+#include <modules/storage.h>
+#include <modules/comm/comm.h>
+
+#include "version.h"
 #include "ecu.h"
+#include "car.h"
 
-#define BLINK_THREAD_PRIO (NORMALPRIO - 2)
-THD_WORKING_AREA(blink_thread_area, 32);
+int main(void) {
+    halInit();
+    /** Kernel becomes thread, rtos is activated */
+    chSysInit();
 
-static THD_FUNCTION(Blink_Thread, arg)
-{
-    (void) arg;
+    Comm_Init();
+    Log_Init();
+    Log_Info(LOG_SOURCE_SYSTEM, "ECU is booting...");
+    Log_Debug(LOG_SOURCE_SYSTEM, "SYSCLK=%u", STM32_SYSCLK);
+    Log_Info(LOG_SOURCE_SYSTEM, "Version %d.%d", VER_MAJOR, VER_MINOR);
+    Log_Info(LOG_SOURCE_SYSTEM, "Compiled: %s", __DATE__ "-" __TIME__);
+
+    Wdgd_Init();
+    Storage_LoadAll();
+    ECU_Init();
+    Car_Init();
 
     while (1) {
-        ECU_ValvesMoveFront(true);
-        palSetLine(LINE_LED_SYS_ACTIVE);
-        chThdSleepMilliseconds(500);
-        palClearLine(LINE_LED_SYS_ACTIVE);
-        ECU_ValvesMoveBack(true);
+        palToggleLine(LINE_LED_SYS_ACTIVE);
         chThdSleepMilliseconds(500);
     }
-}
-
-/*
- * Application entry point.
- */
-int main(void) {
-  /*
-   * System initializations.
-   * - HAL initialization, this also initializes the configured device drivers
-   *   and performs the board-specific initializations.
-   * - Kernel initialization, the main() function becomes a thread and the
-   *   RTOS is active.
-   */
-  halInit();
-  chSysInit();
-
-    (void) chThdCreateStatic(blink_thread_area, sizeof(blink_thread_area),
-            BLINK_THREAD_PRIO, Blink_Thread, NULL);
-  /*
-   * Activates the serial driver 2 using the driver default configuration.
-   */
-  sdStart(&SD1, NULL);
-  chprintf((BaseSequentialStream *)&SD1, "SYSCLK=%u\r\n", STM32_SYSCLK);
-
-  /*
-  chprintf(cfg->out, "*** Kernel:       %s\r\n", CH_KERNEL_VERSION);
-  chprintf(cfg->out, "*** Compiled:     %s\r\n", __DATE__ " - " __TIME__);
-#ifdef PORT_COMPILER_NAME
-  chprintf(cfg->out, "*** Compiler:     %s\r\n", PORT_COMPILER_NAME);
-#endif
-  chprintf(cfg->out, "*** Architecture: %s\r\n", PORT_ARCHITECTURE_NAME);
-#ifdef PORT_CORE_VARIANT_NAME
-  chprintf(cfg->out, "*** Core Variant: %s\r\n", PORT_CORE_VARIANT_NAME);
-#endif
-  chprintf(cfg->out, "*** System Clock: %d\r\n", cfg->sysclk);
-#ifdef PORT_INFO
-  chprintf(cfg->out, "*** Port Info:    %s\r\n", PORT_INFO);
-#endif
-#ifdef PLATFORM_NAME
-  chprintf(cfg->out, "*** Platform:     %s\r\n", PLATFORM_NAME);
-#endif
-#ifdef BOARD_NAME
-  chprintf(cfg->out, "*** Test Board:   %s\r\n", BOARD_NAME);
-#endif
-*/
-
-  /*
-   * Normal main() thread activity, in this demo it does nothing except
-   * sleeping in a loop and check the button state.
-   */
-  Config_Reset();
-  Log_Init();
-  Wdgd_Init();
-  //ECU_Init();
-
-
-  while (true) {
-    chThdSleepMilliseconds(500);
-    chprintf((BaseSequentialStream *)&SD1, "SYSCLK=%u\r\n", STM32_SYSCLK);
-  }
 }
 
 /** @} */
